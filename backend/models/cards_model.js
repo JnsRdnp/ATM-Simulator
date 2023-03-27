0c59166622b727d90d95262b333ad1941337ed3c
SakRoi <roininensaku@gmail.com>
2023-03-27 14:01:58 +0300

    FEAT & BUILD: Add bcryptijs and getAll function

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -1,1 +1,9 @@
 const db = require('../database');
+const bcrypt = require('bcryptijs');
+
+const saltrounds=20;
+const cards={
+    getAll: function(callback) {
+        return db.query('select * from user_table' callback);
+    }
+}
