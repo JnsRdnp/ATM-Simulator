479e62a1d686ff9cd6318b623fb64e553d794d7c
SakRoi <roininensaku@gmail.com>
2023-03-27 14:04:09 +0300

    FEAT: Add getById function

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -4,6 +4,10 @@ const bcrypt = require('bcryptijs');
 const saltrounds=20;
 const cards={
     getAll: function(callback) {
-        return db.query('select * from user_table' callback);
-    }
+        return db.query('select * from cards' callback);
+    },
+
+    getById: function(id, callback) {
+        return db.query('select * from cards where idcard=?', [id], callback)
+    },
 }
