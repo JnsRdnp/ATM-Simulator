20339eb35af072e15af773fc2647408aeb5ac399
SakRoi <roininensaku@gmail.com>
2023-03-27 14:27:45 +0300

    FEAT: Add add function with hashed PINcodes

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -1,13 +1,20 @@
 const db = require('../database');
 const bcrypt = require('bcryptijs');
 
-const saltrounds=20;
+const saltRounds=20;
 const cards={
     getAll: function(callback) {
-        return db.query('select * from cards' callback);
+        return db.query('select * from cards', callback);
     },
 
     getById: function(id, callback) {
-        return db.query('select * from cards where idcard=?', [id], callback)
+        return db.query('select * from cards where idcard=?', [id], callback);
+    },
+
+    add: function(user, callback) {
+        bcrypt.hash(cards.PINcode, saltRounds, function(error, hash){
+            return db.query('insert into cards (idcard, PINcode) values (?,?)',
+            [cards.idCard, hash], callback);
+        });
     },
 }
