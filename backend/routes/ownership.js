c78acc08a5d9f910e59953e92a6b100d836ebde4
Lauri Tuovinen <lare.tuovinen@gmail.com>
2023-03-28 13:51:57 +0300

    added model

--- backend/routes/ownership.js
+++ backend/routes/ownership.js
@@ -2,8 +2,5 @@ const express = require('express');
 const router = express.Router();
 const onwership=require('../models/ownership_model');
 
-router.get('/', functions(req, res, next){
-res.send('message to website'+process.env.SQlserver);
-});
 
 module.exports = router;
