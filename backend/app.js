053d1b3c50cd02a5b257680c1c504c85ef38ec1b
SakRoi <roininensaku@gmail.com>
2023-03-28 13:33:50 +0300

    FEAT: Add link to cards router

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/cards',cardsRouter);
 app.use('/users', usersRouter);
 app.use('/accounts',accountsRouter);
 
