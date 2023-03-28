029d92b38a9918ae6f395bec2136f18826ec4167
santtuniskanen <santtuniska02@gmail.com>
2023-03-28 09:46:51 +0300

    Working on getAll ownerships

--- backend/models/ownership_model.js
+++ backend/models/ownership_model.js
@@ -1,7 +1,10 @@
 const db=require('../database');
 
 const ownership={
-    
+    getAll:function(callback) {
+        return db.query('select * from ownership;'
+        , callback);
+    }
 }
 
 module.exports=ownership;
--- backend/routes/ownership.js
+++ backend/routes/ownership.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const ownership = require('../models/ownership_model');
 const router = express.Router();
-const onwership=require('../models/ownership_model');
 
-module.exports=controller;
+router.get('/',
+    function(request, response){
+        ownership.getAll(function(error, dbResult) {
+            if (error) {
+                response.json(error);
+            } else {
+                console.log(dbResult);
+                response.json(dbResult);
+            }
+        });
+    });
+
+module.exports=router;
