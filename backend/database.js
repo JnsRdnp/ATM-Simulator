5b17b2857c6f8d2deb45b116fe0c69e6c3bda29f
SakRoi <roininensaku@gmail.com>
2023-03-29 13:14:45 +0300

    FIX: Fix database extra dotenv requirement

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,5 @@
 const dotenv = require('dotenv');
 const mysql=require('mysql2');
-
-const dotenv=require('dotenv');
 dotenv.config();
 const connection = mysql.createPool(process.env.SQL_SERVER);
 
