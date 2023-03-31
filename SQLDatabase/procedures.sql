10e89b6ff9cf38b38de6e525f42b604e36df2786
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-03-31 10:44:12 +0300

    debit withdraw procedure

--- SQLDatabase/procedures.sql
+++ SQLDatabase/procedures.sql
@@ -1,7 +1,7 @@
 DELIMITER //
 CREATE PROCEDURE fetchName(IN accountsid int)
 BEGIN
-SELECT CONCAT(firstname," ",lastname) as wholeName
+SELECT  CONCAT(firstname," ",lastname) into @wholeName
 FROM user
 WHERE iduser =
 ( SELECT user_iduser
@@ -14,3 +14,21 @@ WHERE iduser =
 END //
 DELIMITER ;
 
+
+DELIMITER //
+CREATE PROCEDURE debit_withdraw(IN accountID INT,IN amount float)
+BEGIN
+
+  DECLARE test1 INT DEFAULT 0;
+  START TRANSACTION;
+  call fetchName(accountID);
+  UPDATE accounts SET balance=balance-amount WHERE idaccounts=accountID AND balance>=amount;
+  SET test1=ROW_COUNT();
+    IF (test1 > 0) THEN COMMIT;
+	  INSERT INTO history(accounts_idaccounts,wholeName,withdrawal,date)
+	  VALUES(accountID,@wholeName,amount,NOW());
+    ELSE
+      ROLLBACK;
+  END IF;
+END //
+DELIMITER ;
