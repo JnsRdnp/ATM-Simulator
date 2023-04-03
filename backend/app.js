0735128f54edd60c765f6da9faab94abaea7797b
santtuniskanen <santtuniska02@gmail.com>
2023-04-03 14:07:29 +0300

    removed app.use(authenticateToken)

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(authenticateToken);
 
 app.use('/', indexRouter);
 app.use('/cards',cardsRouter); //works as our authetication
