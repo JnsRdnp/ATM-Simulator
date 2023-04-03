84663eef09bd97fb446fb70c3af640eaacdbc240
SakRoi <roininensaku@gmail.com>
2023-04-03 12:16:06 +0300

    FEAT: Add auntentication, Token generation and login functions

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const jtw = require('jsonwebtoken');
+const dotenv = require('dotenv');
 const cards = require("../models/cards_model");
 
 router.get('/',
@@ -58,5 +60,44 @@ function(request, response) {
         }
     });
 });
+
+router.post('/login',
+function(request, response) {
+    if (request.body.id && request.body.PINcode) {
+        const id = request.body.id;
+        const PINcode = request.body.PINcode;
+
+        cards.checkPIN(user, function(dbError, dbResult) {
+            if(dbError) {
+                response.json(dbError);
+            } else {
+                if (dbResult > 0) {
+                    bcrypt.compare(givenPIN, dbResult[0].PINcode, function(error, compareResult) {
+                        if (compareResult){
+                            console.log("succes");
+                            const token = generateAccessToken({ id: user});
+                            response.send(token);
+                        } else {
+                            console.log("wrong password");
+                            response.send(false);
+                        }
+                    });
+                } else {
+                    console.log("Card does not exist");
+                    response.send(false);
+                }
+            }
+        });
+    }
+    else {
+        console.log("Card number or PIN code missing");
+        response.send(false);
+    }
+});
+
+function generateAccessToken(id) {
+    dotenv.config();
+    return jtw.sign(id, process.env.MY_TOKEN, { expiresIn: '200s'});
+}
 
 module.exports = router;
