424924fb459a213db777262d08896482c06b9a31
santtuniskanen <santtuniska02@gmail.com>
2023-04-03 13:37:35 +0300

    added create_token

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(authenticateToken);
 
 app.use('/', indexRouter);
 app.use('/cards',cardsRouter); //works as our authetication
@@ -51,7 +52,7 @@ function authenticateToken(request, response, next) {
         console.log(error);
   
         if (error){
-            return res.sendStatus(403);
+            return response.sendStatus(403);
         }
         request.user = user;
         next();
---
+++ backend/create_token.js
@@ -0,0 +1,14 @@
+
+// https://peatutor.com/express/Examples/webtoken.php
+var crypto = require('crypto');
+var co = require('co');
+
+function spawnTokenBuf() {
+    return function(callback) {
+        crypto.randomBytes(64, callback);
+    };
+}
+
+co(function* () {
+    console.log((yield spawnTokenBuf()).toString('base64'));
+});
