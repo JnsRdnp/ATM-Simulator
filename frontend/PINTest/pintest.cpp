436d5aaf78d4c8b0fa584bf063d04e717293e0f7
santtuniskanen <santtuniska02@gmail.com>
2023-04-04 23:42:45 +0300

    adding pintest

---
+++ frontend/PINTest/pintest.cpp
@@ -0,0 +1,6 @@
+#include "pintest.h"
+
+PINTest::PINTest()
+{
+
+}
---
+++ frontend/PINTest/pintest.h
@@ -0,0 +1,13 @@
+#ifndef PINTEST_H
+#define PINTEST_H
+
+#include <QObject>
+
+class PINTest : public QObject
+{
+    Q_OBJECT
+public:
+    PINTest();
+};
+
+#endif // PINTEST_H
