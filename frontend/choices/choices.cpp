6db38dc3ae5365c7efe1edf6e0f042fb2f69a198
SakRoi <roininensaku@gmail.com>
2023-04-12 14:41:19 +0300

    FEAT: Try to make bit objects into something readable, fail miserably

--- frontend/choices/choices.cpp
+++ frontend/choices/choices.cpp
@@ -1,5 +1,53 @@
 #include "choices.h"
 
-Choices::Choices()
+Choices::Choices(QWidget *parent, QString inPIN, QString inCardID, QByteArray inJWT) :
+    QDialog(parent)
 {
+    PIN = inPIN;
+    cardID = inCardID;
+    JWT = inJWT;
+
+    //networking code
+    QString site_url="http://localhost:3000/cards/" + cardID;
+    QNetworkRequest request((site_url));
+    QByteArray myJWToken="Bearer "+ JWT;
+    request.setRawHeader(QByteArray("Authorization"),(myJWToken));
+    getManager = new QNetworkAccessManager(this);
+
+    connect(getManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(getCardInfo(QNetworkReply*)));
+
+    reply = getManager->get(request);
+}
+
+void Choices::getCardInfo(QNetworkReply *reply)
+{
+    //original source: https://peatutor.com/qt/http_get.php, refactored by Saku Roininen
+    //response_data=reply->readAll();
+    //qDebug()<<"DATA : "+response_data;
+
+    QString test = "{\"bookHeavyInfo\":{\"Qty\":100},\"bookLightInfo\":{\"Qty\":2}}";
+
+    //QJsonDocument jsonResponse = QJsonDocument::fromJson(test.toUtf8());
+    //QJsonObject jsonObject = jsonResponse.object();
+    //QJsonObject bookHeavyInfo = jsonObject["bookHeavyInfo"].toObject();
+    //int qty = bookHeavyInfo["Qty"].toInt();
+    //QJsonObject = jsonObject["credit"].toObject();
+    //QJsonArray card = jsonCredit["data"].toArray();
+    //qDebug()<< qty;
+    //bool credit = card[0].toBool();
+    /*if(card[0]==1)
+    {
+        qDebug()<<"Toimii";
+    }
+    else
+    {
+        qDebug()<<"Ei Toimi";
+    }for (int i = 0; i < json_array.size(); i++)
+    {
+        QJsonObject object = json_array[i].toObject();
+
+    }*/
+    //qDebug() << card;
+    reply->deleteLater();
+    getManager->deleteLater();
 }
--- frontend/choices/choices.h
+++ frontend/choices/choices.h
@@ -3,12 +3,27 @@
 
 #include "choices_global.h"
 #include "QDialog"
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 
 class CHOICES_EXPORT Choices : public QDialog
 {
     Q_OBJECT
 public:
-    Choices();
+    Choices(QWidget *parent = nullptr, QString inPIN = "", QString inCardID = "", QByteArray inJWT = "");
+
+private slots:
+    void getCardInfo (QNetworkReply *reply);
+
+private:
+    QString PIN = "";
+    QString cardID = "";
+    QByteArray JWT = "";
+    //for networking and getting card info
+    QNetworkAccessManager *getManager;
+    QNetworkReply *reply;
+    QByteArray response_data;
 };
 
 #endif // CHOICES_H
